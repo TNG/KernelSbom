9aef5465d0194f524c85ab1bc67e81eeba61aea2
Luis Augenstein <luis.augenstein@tngtech.com>
2025-09-19 13:29:07 +0000

    completed spdx getting started example
    Signed-off-by: Luis Augenstein <luis.augenstein@tngtech.com>

--- src/lib/sbom/spdx.py
+++ src/lib/sbom/spdx.py
@@ -16,6 +16,12 @@ def _generate_spdx_id(entity_type: str) -> SpdxId:
     return f"https://spdx.org/spdxdocs/{entity_type}-{uuid.uuid4()}"
 
 
+def _to_dict(obj: Any, allow_custom_to_dict: bool = True) -> dict[str, Any]:
+    if hasattr(obj, "to_dict") and allow_custom_to_dict:
+        return obj.to_dict()
+    return {k: v for k, v in asdict(obj).items() if v}
+
+
 @dataclass(kw_only=True)
 class Hash:
     type: str = field(init=False, default="Hash")
@@ -48,7 +54,7 @@ class Person(Element):
 @dataclass(kw_only=True)
 class CreationInfo:
     type: str = field(init=False, default="CreationInfo")
-    id: str = "_:creationinfo"  # will serialize as '@id'
+    spdxId: SpdxId = "_:creationinfo"
     specVersion: str = SPDX_SPEC_VERSION
     createdBy: list[SpdxId]
     created: str = field(default_factory=lambda: datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"))
@@ -62,28 +68,29 @@ class SpdxDocument(Element):
     rootElement: list[SpdxId] = field(default_factory=list[SpdxId])
 
 
-# @dataclass(kw_only=True)
-# class SoftwarePackage(Element):
-#     type: str = field(init=False, default="software_Package")
-#     spdxId: SpdxId = field(default_factory=lambda: _generate_spdx_id("Package"))
-#     name: str
-#     software_packageVersion: Optional[str] = None
-#     software_downloadLocation: Optional[str] = None
-#     builtTime: Optional[str] = None
-#     originatedBy: list[SpdxId] = field(default_factory=list)
-#     verifiedUsing: list[Hash] = field(default_factory=list)
+@dataclass(kw_only=True)
+class SoftwarePackage(Element):
+    type: str = field(init=False, default="software_Package")
+    spdxId: SpdxId = field(default_factory=lambda: _generate_spdx_id("Package"))
+    name: str
+    software_packageVersion: Optional[str] = None
+    software_downloadLocation: Optional[str] = None
+    builtTime: Optional[str] = None
+    originatedBy: list[SpdxId] = field(default_factory=list[SpdxId])
+    verifiedUsing: list[Hash] = field(default_factory=list[Hash])
 
-# @dataclass(kw_only=True)
-# class SoftwareFile(Element):
-#     type: str = field(init=False, default="software_File")
-#     spdxId: SpdxId = field(default_factory=lambda: _generate_spdx_id("File"))
-#     name: str
-#     verifiedUsing: list[Hash] = field(default_factory=list)
-#     builtTime: Optional[str] = None
-#     originatedBy: list[SpdxId] = field(default_factory=list)
-#     software_primaryPurpose: Optional[str] = None
-#     software_additionalPurpose: list[str] = field(default_factory=list)
-#     software_copyrightText: Optional[str] = None
+
+@dataclass(kw_only=True)
+class SoftwareFile(Element):
+    type: str = field(init=False, default="software_File")
+    spdxId: SpdxId = field(default_factory=lambda: _generate_spdx_id("File"))
+    name: str
+    verifiedUsing: list[Hash] = field(default_factory=list[Hash])
+    builtTime: Optional[str] = None
+    originatedBy: list[SpdxId] = field(default_factory=list[SpdxId])
+    software_primaryPurpose: Optional[str] = None
+    software_additionalPurpose: list[str] = field(default_factory=list[str])
+    software_copyrightText: Optional[str] = None
 
 
 @dataclass(kw_only=True)
@@ -91,9 +98,20 @@ class Relationship(Element):
     type: str = field(init=False, default="Relationship")
     spdxId: SpdxId = field(default_factory=lambda: _generate_spdx_id("Relationship"))
     relationshipType: str
-    from_: SpdxId = field(metadata={"name": "from"})  # reserved keyword workaround
+    from_: SpdxId  # underscore because 'from' is a reserved keyword
     to: list[SpdxId]
     completeness: Optional[str] = None
+
+    def to_dict(self) -> dict[str, Any]:
+        d = _to_dict(self, allow_custom_to_dict=False)
+        d["from"] = d.pop("from_")
+        return d
+
+
+@dataclass(kw_only=True)
+class RelationShipContains(Relationship):
+    spdxId: SpdxId = field(default_factory=lambda: _generate_spdx_id("Relationship/contains"))
+    relationshipType: str = field(init=False, default="contains")
 
 
 @dataclass(kw_only=True)
@@ -112,6 +130,6 @@ class JsonLdDocument:
 
     def to_json(self):
         return json.dumps(
-            {"@context": self.context, "@graph": [asdict(item) for item in self.graph]},
+            {"@context": self.context, "@graph": [_to_dict(item) for item in self.graph]},
             indent=2,
         )
