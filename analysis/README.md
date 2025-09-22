c71f3857b8c70057f64826bc2b33ea90126c16c8
Luis Augenstein <luis.augenstein@tngtech.com>
2025-09-22 14:34:17 +0000

    added force-graph setup
    Signed-off-by: Luis Augenstein <luis.augenstein@tngtech.com>

---
+++ analysis/README.md
@@ -0,0 +1,35 @@
+<!--
+SPDX-FileCopyrightText: 2025 TNG Technology Consulting GmbH
+
+SPDX-License-Identifier: GPL-2.0-only
+-->
+
+# SBOM Analysis
+
+This directory contains scripts to analyze the **cmd graph** created by the main [`sbom`](../sbom) project.
+
+> 💡 **Note:** The scripts assume that the `linux` source tree is located next to this repository. This is the default layout when using the [devcontainer](../.devcontainer/devcontainer.json).
+
+
+## Build Kernel Based on Cmd Graph
+
+The script [`cmd_graph_based_kernel_build.py`](./cmd_graph_based_kernel_build.py) allows you to verify whether the cmd graph is complete — i.e., it includes all necessary source files required to build the kernel.
+
+This script reconstructs a minimal Linux source tree in `linux-cmd/` by copying only the files referenced in the cmd graph from the original `linux` source tree. If the kernel builds successfully, this confirms that no required file is missing from the cmd graph.
+
+```bash
+python analysis/cmd_graph_based_kernel_build.py
+cd linux-cmd
+make defconfig O=kernel-build
+make -j$(nproc) O=kernel-build
+```
+
+## Visualization Cmd Graph
+
+The script [`cmd_graph_visualization.py`](./cmd_graph_visualization.py) saves the cmd graph as a [web/cmd_graph.json](./web/cmd_graph.json) file such that it can be visualized with [force-graph](https://github.com/vasturiano/force-graph) in the [web/index.html](web/index.html).
+
+```bash
+python analysis/cmd_graph_visualization.py
+cd analysis/web
+python -m http.server 8000
+```
--- analysis/cmd_graph_based_kernel_build.py
+++ analysis/cmd_graph_based_kernel_build.py
@@ -35,7 +35,7 @@ def _copy_cmd_graph_sources(
 ) -> None:
     if _is_path_within(cmd_graph.absolute_path, output_tree):
         if depth <= log_graph_depth_limit:
-            logging.info(f"Skip Node: {depth * '  '}{cmd_graph.absolute_path.relative_to(output_tree)}")
+            logging.info(f"Copy Node: {depth * '  '}{cmd_graph.absolute_path.relative_to(output_tree)}")
     else:
         relative_path = cmd_graph.absolute_path.relative_to(src_tree)
         if depth <= log_graph_depth_limit:
@@ -48,19 +48,21 @@ def _copy_cmd_graph_sources(
         _copy_cmd_graph_sources(child_node, output_tree, src_tree, cmd_src_tree, depth + 1)
 
 
-def _copy_additional_build_files(src_tree: Path, cmd_src_tree: Path) -> None:
+def _copy_additional_build_files(src_tree: Path, cmd_src_tree: Path, output_tree: Path) -> None:
     patterns = [
-        "scripts/**/*",
         "Makefile",
         "Kconfig",
         "Kconfig.*",
+        "Kbuild",
         "arch/x86/configs/x86_64_defconfig",
+        "scripts/**/*",
+        "tools/**/*",
     ]
 
     for pattern in patterns:
         logging.info(f"Copy {pattern}")
         for file_path in src_tree.rglob(pattern):
-            if not file_path.is_file():
+            if not file_path.is_file() or _is_path_within(file_path, output_tree):
                 continue
             print(file_path)
             dest_path = cmd_src_tree / file_path.relative_to(src_tree)
@@ -68,13 +70,9 @@ def _copy_additional_build_files(src_tree: Path, cmd_src_tree: Path) -> None:
             shutil.copy2(file_path, dest_path)
 
 
-def _build_linux_kernel(cmd_src_tree: Path, cmd_output_tree: Path) -> None:
-    pass
-
-
 if __name__ == "__main__":
     # Paths to the original source and build directories
-    cmd_graph_path = Path("evaluation/cmd_graph.pickle")
+    cmd_graph_path = Path("analysis/cmd_graph.pickle")
     src_tree = Path("../linux").resolve()
     output_tree = Path("../linux/kernel-build").resolve()
     root_output_in_tree = Path("vmlinux")
@@ -83,18 +81,14 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
 
     # Load cached command graph if available, otherwise build it from .cmd files
-    # if cmd_graph_path.exists():
-    #     logging.info("Loading cmd graph")
-    #     cmd_graph = load_cmd_graph(cmd_graph_path)
-    # else:
-    #     cmd_graph = build_cmd_graph(root_output_in_tree, output_tree, src_tree)
-    #     save_cmd_graph(cmd_graph, cmd_graph_path)
+    if cmd_graph_path.exists():
+        logging.info("Loading cmd graph")
+        cmd_graph = load_cmd_graph(cmd_graph_path)
+    else:
+        cmd_graph = build_cmd_graph(root_output_in_tree, output_tree, src_tree)
+        save_cmd_graph(cmd_graph, cmd_graph_path)
 
     # Create a new source tree containing only the files referenced in the command graph
     cmd_src_tree = Path("./linux-cmd").resolve()
-    # _copy_cmd_graph_sources(cmd_graph, output_tree, src_tree, cmd_src_tree)
-    _copy_additional_build_files(src_tree, cmd_src_tree)
-
-    # Copy additional required build files and rebuild the Linux kernel from the reduced source tree
-    cmd_output_tree = Path("./linux-cmd/kernel-build").resolve()
-    _build_linux_kernel(cmd_src_tree, cmd_output_tree)
+    _copy_cmd_graph_sources(cmd_graph, output_tree, src_tree, cmd_src_tree)
+    _copy_additional_build_files(src_tree, cmd_src_tree, output_tree)
