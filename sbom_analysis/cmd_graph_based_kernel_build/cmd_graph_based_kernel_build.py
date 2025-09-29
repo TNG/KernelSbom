c621260ccc0faedeb222e0f92e5079439e217939
Luis Augenstein <luis.augenstein@tngtech.com>
2025-09-29 13:44:38 +0000

    update cmd graph based kernel build script to automatically create list of missing files
    Signed-off-by: Luis Augenstein <luis.augenstein@tngtech.com>

--- sbom_analysis/cmd_graph_based_kernel_build/cmd_graph_based_kernel_build.py
+++ sbom_analysis/cmd_graph_based_kernel_build/cmd_graph_based_kernel_build.py
@@ -9,6 +9,7 @@ import logging
 import os
 import re
 import shutil
+import subprocess
 import sys
 from pathlib import Path
 
@@ -52,6 +53,34 @@ def _remove_files(base_path: Path, patterns_to_remove: list[re.Pattern[str]], ig
     return removed_files
 
 
+def run_command_live(cmd: list[str], cwd: Path) -> tuple[int, list[str]]:
+    proc = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, bufsize=1)
+    output: list[str] = []
+    for line in proc.stdout:  # type: ignore
+        print(line, end="")  # print live without extra newline
+        output.append(line)
+    proc.wait()
+    return proc.returncode, output
+
+
+def _build_kernel(cwd: Path) -> Path | None:
+    """
+    Builds the kernel using the default config. If an error occurs it returns the missing file that caused the build error.
+    """
+    returncode, output = run_command_live(["make", "defconfig", "O=kernel_build"], cwd)
+    if returncode != 0:
+        match = re.search(r"No rule to make target '([^']+)'", "".join(output))
+        if match:
+            missing_file = match.group(1)
+            return Path(missing_file)
+        raise RuntimeError("Failed to create defconfig")
+
+    returncode, _ = run_command_live(["make", "O=kernel_build"], cwd)
+
+    returncode, output = run_command_live(["make", "O=kernel_build"], cwd)
+    print(output)
+
+
 if __name__ == "__main__":
     script_path = Path(__file__).parent
     # Paths to the original source and build directories
@@ -71,7 +100,7 @@ if __name__ == "__main__":
     shutil.copytree(src_tree, cmd_src_tree, ignore=shutil.ignore_patterns(output_tree.relative_to(src_tree)))
     shutil.rmtree(cmd_src_tree / ".git")
 
-    # Load cached command graph if available, otherwise build it from .cmd files
+    # Load cached command graph or build it from .cmd files
     cmd_graph = build_or_load_cmd_graph(root_output_in_tree, output_tree, src_tree, cmd_graph_path)
 
     # remove source files not in cmd_graph
@@ -82,106 +111,121 @@ if __name__ == "__main__":
     ]
     logging.info("Extract source files from cmd graph")
     cmd_graph_sources = [s.relative_to(src_tree) for s in _get_files_in_cmd_graph(cmd_graph, source_patterns)]
-    with open(script_path / "cmd_graph_sources.json", "wt") as f:
-        json.dump([str(s) for s in cmd_graph_sources], f)
 
-    additional_sources: list[Path] = [
-        s.relative_to(src_tree)
-        for s in [
-            *_get_files_in_directory(src_tree / "scripts", source_patterns),
-            *_get_files_in_directory(src_tree / "tools", source_patterns),
-            src_tree / "arch/x86/tools/relocs_32.c",
-            src_tree / "arch/x86/tools/relocs.h",
-            src_tree / "arch/x86/tools/relocs.c",
-            src_tree / "arch/x86/tools/relocs_64.c",
-            src_tree / "arch/x86/tools/relocs_common.c",
-            *_get_files_in_directory(src_tree / "include/asm-generic", source_patterns),
-            # src_tree / "include/asm-generic/fprobe.h",
-            # src_tree / "include/asm-generic/dma-mapping.h",
-            # src_tree / "include/asm-generic/module.lds.h",
-            # src_tree / "include/asm-generic/xor.h",
-            # ...
-            src_tree / "include/linux/kbuild.h",
-            src_tree / "arch/x86/kernel/asm-offsets.c",
-            src_tree / "include/crypto/aria.h",
-            src_tree / "arch/x86/kernel/asm-offsets_64.c",
-            src_tree / "arch/x86/entry/vdso/vdso.lds.S",
-            src_tree / "arch/x86/entry/vdso/vdso-layout.lds.S",
-            src_tree / "arch/x86/entry/vdso/vdso2c.c",
-            src_tree / "arch/x86/entry/vdso/vdso2c.h",
-            src_tree / "arch/x86/entry/vdso/vdso32/vdso32.lds.S",
-            *_get_files_in_directory(src_tree / "arch/x86/realmode/rm", source_patterns),
-            # src_tree / "arch/x86/realmode/rm/header.S",
-            # src_tree / "arch/x86/realmode/rm/realmode.h",
-            # src_tree / "arch/x86/realmode/rm/trampoline_64.S",
-            # src_tree / "arch/x86/realmode/rm/trampoline_common.S",
-            # src_tree / "arch/x86/realmode/rm/stack.S",
-            # src_tree / "arch/x86/realmode/rm/reboot.S",
-            # src_tree / "arch/x86/realmode/rm/wakeup_asm.S",
-            # src_tree / "arch/x86/realmode/rm/wakemain.c",
-            # src_tree / "arch/x86/realmode/rm/video-mode.c",
-            # ...
-            *_get_files_in_directory(src_tree / "arch/x86/boot", source_patterns),
-            # src_tree / "arch/x86/boot/boot.h",
-            # src_tree / "arch/x86/boot/bitops.h",
-            # src_tree / "arch/x86/boot/ctype.h",
-            # src_tree / "arch/x86/boot/cpuflags.h",
-            # src_tree / "arch/x86/boot/io.h",
-            # src_tree / "arch/x86/boot/video-mode.c",
-            # ....
-            *_get_files_in_directory(src_tree / "arch/x86/include", source_patterns),
-            *_get_files_in_directory(src_tree / "arch/x86/kernel", source_patterns),
-            src_tree / "kernel/bounds.c",
-            src_tree / "usr/gen_init_cpio.c",
-            src_tree / "certs/extract-cert.c",
-            src_tree / "fs/efivarfs/inode.c",
-            src_tree / "fs/efivarfs/internal.h",
-            src_tree / "fs/efivarfs/file.c",
-            src_tree / "fs/efivarfs/super.c",
-            src_tree / "fs/efivarfs/vars.c",
-            src_tree / "security/selinux/genheaders.c",
-            src_tree / "lib/crc/gen_crc32table.c",
-            src_tree / "drivers/tty/vt/conmakehash.c",
-            src_tree / "drivers/thermal/intel/x86_pkg_temp_thermal.c",
-            *_get_files_in_directory(src_tree / "drivers/firmware/efi/libstub", source_patterns),
-            # src_tree / "drivers/firmware/efi/libstub/alignedmem.c",
-            # src_tree / "drivers/firmware/efi/libstub/efistub.h",
-            # src_tree / "drivers/firmware/efi/libstub/efi-stub-helper.c",
-            # src_tree / "drivers/firmware/efi/libstub/file.c",
-            # src_tree / "drivers/firmware/efi/libstub/gop.c",
-            # src_tree / "drivers/firmware/efi/libstub/mem.c",
-            # src_tree / "drivers/firmware/efi/libstub/pci.c",
-            # src_tree / "drivers/firmware/efi/libstub/printk.c",
-            # src_tree / "drivers/firmware/efi/libstub/random.c",
-            # src_tree / "drivers/firmware/efi/libstub/randomalloc.c",
-            # src_tree / "drivers/firmware/efi/libstub/relocate.c",
-            # src_tree / "drivers/firmware/efi/libstub/secureboot.c",
-            # src_tree / "drivers/firmware/efi/libstub/skip_spaces.c",
-            # src_tree / "drivers/firmware/efi/libstub/smbios.c",
-            # src_tree / "drivers/firmware/efi/libstub/tpm.c",
-            # ...
-            src_tree / "net/netfilter/nf_log_syslog.c",
-            src_tree / "net/ipv4/netfilter/nf_reject_ipv4.c",
-            src_tree / "net/ipv6/netfilter/nf_reject_ipv6.c",
-            src_tree / "net/netfilter/xt_mark.c",
-            src_tree / "net/netfilter/xt_LOG.c",
-            src_tree / "net/netfilter/xt_MASQUERADE.c",
-            src_tree / "net/netfilter/xt_addrtype.c",
-            src_tree / "include/uapi/linux/netfilter/xt_LOG.h",
-            src_tree / "include/net/netfilter/ipv4/nf_reject.h",
-            src_tree / "include/net/netfilter/nf_reject.h",
-            src_tree / "include/net/netfilter/ipv6/nf_reject.h",
-            src_tree / "include/uapi/linux/netfilter/xt_mark.h",
-            src_tree / "include/uapi/linux/netfilter/xt_addrtype.h",
-            src_tree / "include/linux/export-internal.h",
-            src_tree / "include/linux/pe.h",
-        ]
-    ]
+    # additional_sources: list[Path] = [
+    #     s.relative_to(src_tree)
+    #     for s in [
+    #         # *_get_files_in_directory(src_tree / "scripts", source_patterns),
+    #         # *_get_files_in_directory(src_tree / "tools", source_patterns),
+    #         src_tree / "arch/x86/tools/relocs_32.c",
+    #         src_tree / "arch/x86/tools/relocs.h",
+    #         src_tree / "arch/x86/tools/relocs.c",
+    #         src_tree / "arch/x86/tools/relocs_64.c",
+    #         src_tree / "arch/x86/tools/relocs_common.c",
+    #         # *_get_files_in_directory(src_tree / "include/asm-generic", source_patterns),
+    #         src_tree / "include/asm-generic/fprobe.h",
+    #         src_tree / "include/asm-generic/dma-mapping.h",
+    #         src_tree / "include/asm-generic/module.lds.h",
+    #         src_tree / "include/asm-generic/xor.h",
+    #         # ...
+    #         src_tree / "include/linux/kbuild.h",
+    #         src_tree / "arch/x86/kernel/asm-offsets.c",
+    #         src_tree / "include/crypto/aria.h",
+    #         src_tree / "arch/x86/kernel/asm-offsets_64.c",
+    #         src_tree / "arch/x86/entry/vdso/vdso.lds.S",
+    #         src_tree / "arch/x86/entry/vdso/vdso-layout.lds.S",
+    #         src_tree / "arch/x86/entry/vdso/vdso2c.c",
+    #         src_tree / "arch/x86/entry/vdso/vdso2c.h",
+    #         src_tree / "arch/x86/entry/vdso/vdso32/vdso32.lds.S",
+    #         # *_get_files_in_directory(src_tree / "arch/x86/realmode/rm", source_patterns),
+    #         src_tree / "arch/x86/realmode/rm/header.S",
+    #         src_tree / "arch/x86/realmode/rm/realmode.h",
+    #         src_tree / "arch/x86/realmode/rm/trampoline_64.S",
+    #         src_tree / "arch/x86/realmode/rm/trampoline_common.S",
+    #         src_tree / "arch/x86/realmode/rm/stack.S",
+    #         src_tree / "arch/x86/realmode/rm/reboot.S",
+    #         src_tree / "arch/x86/realmode/rm/wakeup_asm.S",
+    #         src_tree / "arch/x86/realmode/rm/wakemain.c",
+    #         src_tree / "arch/x86/realmode/rm/video-mode.c",
+    #         # ...
+    #         # *_get_files_in_directory(src_tree / "arch/x86/boot", source_patterns),
+    #         src_tree / "arch/x86/boot/boot.h",
+    #         src_tree / "arch/x86/boot/bitops.h",
+    #         src_tree / "arch/x86/boot/ctype.h",
+    #         src_tree / "arch/x86/boot/cpuflags.h",
+    #         src_tree / "arch/x86/boot/io.h",
+    #         src_tree / "arch/x86/boot/video-mode.c",
+    #         # ....
+    #         # *_get_files_in_directory(src_tree / "arch/x86/include", source_patterns),
+    #         # *_get_files_in_directory(src_tree / "arch/x86/kernel", source_patterns),
+    #         src_tree / "kernel/bounds.c",
+    #         src_tree / "usr/gen_init_cpio.c",
+    #         src_tree / "certs/extract-cert.c",
+    #         src_tree / "fs/efivarfs/inode.c",
+    #         src_tree / "fs/efivarfs/internal.h",
+    #         src_tree / "fs/efivarfs/file.c",
+    #         src_tree / "fs/efivarfs/super.c",
+    #         src_tree / "fs/efivarfs/vars.c",
+    #         src_tree / "security/selinux/genheaders.c",
+    #         src_tree / "lib/crc/gen_crc32table.c",
+    #         src_tree / "drivers/tty/vt/conmakehash.c",
+    #         src_tree / "drivers/thermal/intel/x86_pkg_temp_thermal.c",
+    #         # *_get_files_in_directory(src_tree / "drivers/firmware/efi/libstub", source_patterns),
+    #         src_tree / "drivers/firmware/efi/libstub/alignedmem.c",
+    #         src_tree / "drivers/firmware/efi/libstub/efistub.h",
+    #         src_tree / "drivers/firmware/efi/libstub/efi-stub-helper.c",
+    #         src_tree / "drivers/firmware/efi/libstub/file.c",
+    #         src_tree / "drivers/firmware/efi/libstub/gop.c",
+    #         src_tree / "drivers/firmware/efi/libstub/mem.c",
+    #         src_tree / "drivers/firmware/efi/libstub/pci.c",
+    #         src_tree / "drivers/firmware/efi/libstub/printk.c",
+    #         src_tree / "drivers/firmware/efi/libstub/random.c",
+    #         src_tree / "drivers/firmware/efi/libstub/randomalloc.c",
+    #         src_tree / "drivers/firmware/efi/libstub/relocate.c",
+    #         src_tree / "drivers/firmware/efi/libstub/secureboot.c",
+    #         src_tree / "drivers/firmware/efi/libstub/skip_spaces.c",
+    #         src_tree / "drivers/firmware/efi/libstub/smbios.c",
+    #         src_tree / "drivers/firmware/efi/libstub/tpm.c",
+    #         # ...
+    #         src_tree / "net/netfilter/nf_log_syslog.c",
+    #         src_tree / "net/ipv4/netfilter/nf_reject_ipv4.c",
+    #         src_tree / "net/ipv6/netfilter/nf_reject_ipv6.c",
+    #         src_tree / "net/netfilter/xt_mark.c",
+    #         src_tree / "net/netfilter/xt_LOG.c",
+    #         src_tree / "net/netfilter/xt_MASQUERADE.c",
+    #         src_tree / "net/netfilter/xt_addrtype.c",
+    #         src_tree / "include/uapi/linux/netfilter/xt_LOG.h",
+    #         src_tree / "include/net/netfilter/ipv4/nf_reject.h",
+    #         src_tree / "include/net/netfilter/nf_reject.h",
+    #         src_tree / "include/net/netfilter/ipv6/nf_reject.h",
+    #         src_tree / "include/uapi/linux/netfilter/xt_mark.h",
+    #         src_tree / "include/uapi/linux/netfilter/xt_addrtype.h",
+    #         src_tree / "include/linux/export-internal.h",
+    #         src_tree / "include/linux/pe.h",
+    #     ]
+    # ]
+    missing_sources_in_cmd_graph: list[Path] = []
+    if (script_path / "missing_sources_in_cmd_graph.json").exists():
+        with open(script_path / "missing_sources_in_cmd_graph.json", "r") as f:
+            missing_sources_in_cmd_graph = json.load(f)
+
     logging.info("Remove source files not in cmd graph")
-    removed_sources = _remove_files(
+    _remove_files(
         cmd_src_tree,
         patterns_to_remove=source_patterns,
-        ignore=set(cmd_graph_sources + additional_sources),
+        ignore=set(cmd_graph_sources + missing_sources_in_cmd_graph),
     )
-    with open(script_path / "removed_sources.json", "wt") as f:
-        json.dump([str(s) for s in removed_sources], f)
+
+    while True:
+        logging.info("Attempting to build kernel")
+        missing_source = _build_kernel(cmd_src_tree)
+        if missing_source is None:
+            logging.info("Successfully built kernel")
+            break
+
+        logging.info(f"Found missing source: {missing_source}.")
+        shutil.copy2(src_tree / missing_source, cmd_src_tree / missing_source)
+        missing_sources_in_cmd_graph.append(src_tree / missing_source)
+
+        with open(script_path / "missing_sources_in_cmd_graph.json", "wt") as f:
+            json.dump([str(missing_file) for missing_file in missing_sources_in_cmd_graph], f)
